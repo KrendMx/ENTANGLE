9f190eaa704ec40498d4d298936596f7447a973d
Krkmx41 <maximha4@gmail.com>
2022-08-11 13:25:31 +0300

    Fixes

--- src/UI/Components/Home.Components/PayModal/index.tsx
+++ src/UI/Components/Home.Components/PayModal/index.tsx
@@ -116,8 +116,8 @@ const PayModal: React.FC<PayModalPropsType> = ({
             Number(
                 (
                     await chainThingsSynth.contract.balanceOf(account)
-                ).toBigInt()
-                    / BigInt(10 ** (await chainThingsSynth.contract.decimals())),
+                )
+                    / (10 ** (await chainThingsSynth.contract.decimals())),
             ),
         [account, txLoading, chainId],
     );
