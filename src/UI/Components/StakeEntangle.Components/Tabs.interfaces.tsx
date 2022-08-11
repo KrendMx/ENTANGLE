1e12c8b30ef8f8282029a73ce965154d9aeba618
Krkmx41 <maximha4@gmail.com>
2022-08-11 14:20:01 +0300

    First commit

---
+++ src/UI/Components/StakeEntangle.Components/Tabs.interfaces.tsx
@@ -0,0 +1,25 @@
+interface ILockState {
+    network: string;
+    usdcAmount: string;
+    lockPeriod: number;
+    apr: string;
+    apr24: string;
+    activeButtonRepay: string
+}
+
+interface WithdrawProps {
+    token: string;
+}
+
+interface IWithdrawState {
+    network: string;
+    activeButtonRepay:string;
+    usdcAmount: string;
+    getUsdc: string;
+}
+
+export type {
+    ILockState,
+    WithdrawProps,
+    IWithdrawState,
+};
